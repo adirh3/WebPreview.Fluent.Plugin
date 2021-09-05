761f164c3a54026b677a5dfc30a40924209184eb
Adir <adirh3@gmail.com>
2021-09-06 00:36:50 +0300

    Initial commit

---
+++ WebPreview.Fluent.Plugin/WebResultPreviewControlBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WebPreview.Fluent.Plugin
+{
+    public class WebResultPreviewControlBuilder : IResultPreviewControlBuilder
+    {
+        private readonly WebView _webView;
+
+        public WebResultPreviewControlBuilder()
+        {
+            _webView = new WebView();
+        }
+
+        public PreviewBuilderDescriptor PreviewBuilderDescriptor { get; } = new()
+        {
+            Name = "Web", Description = "Preview of web pages", ShowPreviewAutomatically = true
+        };
+
+        public bool CanBuildPreviewForResult(ISearchResult searchResult)
+        {
+            return searchResult.Context?.StartsWith("http") ?? false;
+        }
+
+        public ValueTask<Control> CreatePreviewControl(ISearchResult searchResult)
+        {
+            var url = searchResult.Context;
+            _webView.Address = url;
+            return ValueTask.FromResult<Control>(_webView);
+        }
+    }
+}
---
+++ WebPreview.Fluent.Plugin/WebView.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data;
+using Avalonia.Platform;
+
+namespace WebView.Avalonia
+{
+    public class WebView : NativeControlHost
+    {
+        private IntPtr _webViewWindow;
+
+        private static readonly StyledProperty<string> AddressProperty =
+            AvaloniaProperty.Register<WebView, string>(nameof(Address), defaultBindingMode: BindingMode.TwoWay);
+
+        /// <summary>
+        /// The address of the webview
+        /// </summary>
+        public string Address
+        {
+            get => GetValue(AddressProperty);
+            set => SetValue(AddressProperty, value);
+        }
+
+        [DllImport("user32.dll", SetLastError = true, EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLong64b(IntPtr hWnd, int nIndex, IntPtr value);
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr webview_create(int debug, IntPtr window);
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void webview_navigate(IntPtr webview, [MarshalAs(UnmanagedType.LPStr)] string url);
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void webview_destroy(IntPtr webview);
+
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr webview_get_window(IntPtr webview);
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void webview_set_size(
+            IntPtr webview,
+            int width,
+            int height,
+            int hint);
+
+        [DllImport("webview", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void webview_set_title(IntPtr webview, [MarshalAs(UnmanagedType.LPStr)] string title);
+
+        protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
+        {
+            IntPtr intPtr = IntPtr.Zero;
+            _webViewWindow = webview_create(0, intPtr);
+            webview_set_title(_webViewWindow, "bla12345");
+            webview_set_size(_webViewWindow, 1, 1, 0);
+            webview_navigate(_webViewWindow, Address);
+            IntPtr webviewGetWindow = webview_get_window(_webViewWindow);
+            SetWindowLong64b(webviewGetWindow, -16,
+                new IntPtr(0x800000 | 0x10000000 | 0x40000000 | 0x800000 | 0x10000 | 0x0004));
+            return new PlatformHandle(webviewGetWindow, "HWND");
+        }
+
+
+        protected override void DestroyNativeControlCore(IPlatformHandle control)
+        {
+            webview_destroy(_webViewWindow);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            webview_set_size(_webViewWindow, (int)finalSize.Width, (int)finalSize.Height, 0);
+            return base.ArrangeOverride(finalSize);
+        }
+
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+#pragma warning disable 8631
+            base.OnPropertyChanged(change);
+#pragma warning restore 8631
+            if (change.Property == AddressProperty && _webViewWindow != IntPtr.Zero)
+            {
+                webview_navigate(_webViewWindow, Address);
+            }
+        }
+    }
+}
